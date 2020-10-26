c8e088600cd603589593944a614d035d0e1b6619
Alex <alexis.amand@gmail.com>
2020-10-26 17:50:36 +0100

    Ajout d'une page provisoire

---
+++ todo.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Création de site web et SEO | Alexis AMAND - Développeur PHP/Web et Webmaster à Valenciennes</title>
+    <meta name="description" content="Développeur web/PHP à Valenciennes et sa région. Webmaster spécialisé dans la création et la maintenance des sites web pour les TPE, les artisans, les entrepreneurs et les associations.">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="d-flex justify-content-center align-items-center" style="height:100vh">
+
+        <div class="row">
+
+            <div class="col-12">
+            
+            <h1 class="text-center">Alexis AMAND</h1>
+
+            <h5 class="text-center">Developpeur Web/SEO</h5>
+
+            <p class="text-center mt-4">Mon site sera bientôt de retour dans une version mise à jour !<br />
+            En attendant, je vous invite à me retrouver sur les réseaux sociaux<br />
+            ou par mail : alexis.amand[at]gmail.com</p>
+
+            <p class="text-center mt-4">
+                    <a href="https://twitter.com/boitasite" title="lien pour me suivre sur Twitter">
+                    <i class="fi-swluxl-twitter" style="font-size:2em"></i></a>
+                    <a href="https://www.facebook.com/Alexis-AMAND-Webmaster-507308975998494/" title="lien pour me suivre sur Facebook">
+                    <i class="fi-swluxl-facebook" style="font-size:2em"></i></a>
+                    <a href="https://www.linkedin.com/in/alexis-amand-99775817" title="lien pour me suivre sur LinkedIn">
+                    <i class="fi-swluxl-linkedin" style="font-size:2em"></i></a>
+            </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<script defer src="https://friconix.com/cdn/friconix.js"> </script>
+
+</body>
+</html>
