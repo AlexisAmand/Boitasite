292084c155bb6f1ef9e0e9b628b07a47f93d5e36
Alex <alexis.amand@gmail.com>
2025-07-25 10:35:45 +0200

    [maj] services sur la page d'accueil.

--- index.php
+++ index.php
@@ -16,6 +16,20 @@
 
   <meta name='impact-site-verification' value='4db261b3-038a-4dca-ad62-5b3f8e04dc06'>
 
+  <style>
+
+  .bi {
+    transition: transform 0.3s ease, filter 0.3s ease;
+    cursor: pointer; /* Montre que c'est interactif */
+  }
+
+  .bi:hover {
+    transform: scale(1.1);
+    filter: brightness(1.2);
+  }
+
+  </style>
+
 </head>
 <body>
 
@@ -29,6 +43,44 @@
       <a href="services.php" class="btn btn-secondary btn-lg" title="Voir la liste complète de mes services de création de sites web">Découvrez mes services</a>
     </div>
   </header>
+
+  <section class="container my-5">
+  <div class="row">
+    <div class="col-12 col-md-4 mb-4 text-center">
+      <i class="bi bi-laptop display-1 mb-3" style="color:#caa8a0;"></i>
+      <h2 class="my-4 display-6">Sites web sur mesure</h2>
+      <ul class="text-start">
+        <li class="my-3">Blogs, sites vitrines, site de e-commerce.</li>
+        <li class="my-3">Design responsive : nous adaptons nos sites aux environnements smartphones et tablettes (iOS et Android) pour que votre site s'adapte à tous les appareils.</li>
+        <li class="my-3">Gestion facile grâce à des solutions simples.</li>
+        <li class="my-3">Possibilité de contrat de maintenance pour garder votre site web en forme.</li>
+      </ul>
+    </div>
+    <div class="col-12 col-md-4 mb-4 text-center">
+      <i class="bi bi-bar-chart-line display-1 mb-3" style="color:#495e3b;"></i>
+      <h2 class="my-4 display-6">Référencement (SEO)</h2>
+      <ul class="text-start">
+        <li class="my-3"><a href="guide-seo.php" title="Guide complet pour optimiser le référencement naturel">Optimisations SEO</a> de votre site web.</li>
+        <li class="my-3">Audit de votre site pour faire le point sur son référencement.</li>
+        <li class="my-3">Analyse approfondie pour améliorer votre visibilité.</li>
+        <li class="my-3">Stratégies personnalisées et durables.</li>
+        <li class="my-3">Suivi et ajustements réguliers pour booster le trafic.</li>
+      </ul>
+    </div>
+    <div class="col-12 col-md-4 mb-4 text-center">
+      <i class="bi bi-pencil-square display-1 mb-3" style="color:#ced5f2;"></i>
+      <h2 class="my-4 display-6">Rédaction web</h2>
+      <ul class="text-start">
+        <li class="my-3">Textes exclusifs selon vos consignes, avec contrôle du plagiat.</li>
+        <li class="my-3">Textes clairs et optimisés SEO.</li>
+        <li class="my-3">Contenus adaptés à votre secteur.</li>
+        <li class="my-3"><a href="https://lexisio.boitasite.com/" title="Découvrez mes services de rédaction web et création de contenu">Rédaction d'articles</a>, de descriptions, de fiches produits, etc.</li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+
 
   <?php include $_SERVER['DOCUMENT_ROOT'].'/inc/contact.inc.php'; ?>
   <?php include $_SERVER['DOCUMENT_ROOT'].'/inc/footer.inc.php'; ?>
